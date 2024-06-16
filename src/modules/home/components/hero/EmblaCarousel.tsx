c8e2bafb2e30a2c6e713d9f75ddd74301e692085
Abhi <abhishek@rawat.dev>
2024-06-17 00:22:29 +0400

    Storefront - added embla carousel

---
+++ src/modules/home/components/hero/EmblaCarousel.tsx
@@ -0,0 +1,101 @@
+import React, { useCallback, useEffect, useState } from 'react'
+import { EmblaOptionsType } from 'embla-carousel'
+import useEmblaCarousel from 'embla-carousel-react'
+import ClassNames from 'embla-carousel-class-names'
+import {
+  NextButton,
+  PrevButton,
+  usePrevNextButtons
+} from './EmblaCarouselArrowButtons'
+import { DotButton, useDotButton } from './EmblaCarouselDotButton'
+import AutoScroll from 'embla-carousel-auto-scroll'
+
+type PropType = {
+  slides: number[]
+  options?: EmblaOptionsType
+}
+
+const EmblaCarousel: React.FC<PropType> = (props) => {
+  const { slides, options } = props
+  const [emblaRef, emblaApi] = useEmblaCarousel(options, [
+    AutoScroll({ playOnInit: true })
+  ])
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } =
+    useDotButton(emblaApi)
+
+  const {
+    prevBtnDisabled,
+    nextBtnDisabled,
+    onPrevButtonClick,
+    onNextButtonClick
+  } = usePrevNextButtons(emblaApi)
+
+
+  const onButtonAutoplayClick = useCallback(
+    (callback: () => void) => {
+      const autoScroll = emblaApi?.plugins()?.autoScroll
+      if (!autoScroll) return
+
+      const resetOrStop =
+        autoScroll.options.stopOnInteraction === false
+          ? autoScroll.reset
+          : autoScroll.stop
+
+      resetOrStop()
+      callback()
+    },
+    [emblaApi]
+  )
+
+  useEffect(() => {
+    const autoScroll = emblaApi?.plugins()?.autoScroll
+    if (!autoScroll) return
+
+    setIsPlaying(autoScroll.isPlaying())
+    emblaApi
+      .on('autoScroll:play', () => setIsPlaying(true))
+      .on('autoScroll:stop', () => setIsPlaying(false))
+      .on('reInit', () => setIsPlaying(autoScroll.isPlaying()))
+  }, [emblaApi])
+
+  return (
+    <div className="embla content-container">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((index) => (
+            <div className="embla__slide embla__class-names" key={index}>
+              <img
+                className="embla__slide__img"
+                src={`https://picsum.photos/1440/640?v=${index}`}
+                alt="Your alt text"
+              />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={'embla__dot'.concat(
+                index === selectedIndex ? ' embla__dot--selected' : ''
+              )}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default EmblaCarousel
--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,36 +1,32 @@
+"use client"
 import { Github } from "@medusajs/icons"
+import Image from "next/image";
+import EmblaCarousel from './EmblaCarousel'
+import { EmblaOptionsType } from 'embla-carousel'
+import { useState, useEffect, useCallback } from "react";
+import Link from "next/link";
 import { Button, Heading } from "@medusajs/ui"
+import './embla.css'
+
+const slides = [
+  { url: "https://picsum.photos/1200/400" },
+  { url: "https://picsum.photos/1200/400" },
+  { url: "https://picsum.photos/1200/400" },
+  { url: "https://picsum.photos/1200/400" },
+  { url: "https://picsum.photos/1200/400" },
+];
+
+const OPTIONS: EmblaOptionsType = {}
+const SLIDE_COUNT = 5
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+
 
 const Hero = () => {
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
-        <span>
-          <Heading
-            level="h1"
-            className="text-3xl leading-10 text-ui-fg-base font-normal"
-          >
-            Ecommerce Starter Template
-          </Heading>
-          <Heading
-            level="h2"
-            className="text-3xl leading-10 text-ui-fg-subtle font-normal"
-          >
-            Powered by Medusa and Next.js
-          </Heading>
-        </span>
-        <a
-          href="https://github.com/medusajs/nextjs-starter-medusa"
-          target="_blank"
-        >
-          <Button variant="secondary">
-            View on GitHub
-            <Github />
-          </Button>
-        </a>
-      </div>
-    </div>
+    <>
+      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+    </>
   )
 }
-
 export default Hero
+
