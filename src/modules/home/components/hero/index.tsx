890e55c5f61ce6341cb032d7ca42eb9f6e81609d
Abhi <abhishek@rawat.dev>
2024-06-17 02:22:55 +0400

    storefront-adding simple hero image

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,30 +1,38 @@
 "use client"
-import { Github } from "@medusajs/icons"
+
 import Image from "next/image";
-import EmblaCarousel from './EmblaCarousel'
-import { EmblaOptionsType } from 'embla-carousel'
-import { useState, useEffect, useCallback } from "react";
-import Link from "next/link";
-import { Button, Heading } from "@medusajs/ui"
-import './embla.css'
+// import EmblaCarousel from './EmblaCarousel'
+// import { EmblaOptionsType } from 'embla-carousel'
+// import { useState, useEffect, useCallback } from "react";
+// import Link from "next/link";
+// import { Button, Heading } from "@medusajs/ui"
+// import './embla.css'
 
-const slides = [
-  { url: "https://picsum.photos/1200/400" },
-  { url: "https://picsum.photos/1200/400" },
-  { url: "https://picsum.photos/1200/400" },
-  { url: "https://picsum.photos/1200/400" },
-  { url: "https://picsum.photos/1200/400" },
-];
+// const slides = [
+//   { url: "https://picsum.photos/1200/400" },
+//   { url: "https://picsum.photos/1200/400" },
+//   { url: "https://picsum.photos/1200/400" },
+//   { url: "https://picsum.photos/1200/400" },
+//   { url: "https://picsum.photos/1200/400" },
+// ];
 
-const OPTIONS: EmblaOptionsType = {}
-const SLIDE_COUNT = 5
-const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
+// const OPTIONS: EmblaOptionsType = {}
+// const SLIDE_COUNT = 5
+// const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 
 
 const Hero = () => {
   return (
     <>
-      <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      {/* <EmblaCarousel slides={SLIDES} options={OPTIONS} /> */}
+      <Image
+        className="content-container"
+        src="https://picsum.photos/1600/900"
+        width={1600}
+        height={900}
+        alt="Picture of the author">
+      </Image>
+
     </>
   )
 }
